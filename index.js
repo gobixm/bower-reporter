8fb8180d8077ffd3bb4a98791476563373f50011
Kirill Ivanov <gobixm@gmail.com>
2016-06-19 23:22:57 +0300

    default project path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var savePath = args.path ? args.path : 'report.' + report;
 var projPath = args._[0] ? args._[0] : process.cwd();
 
 var ws = fs.createWriteStream(savePath);
-reporter.report(ws, args._[0], report);
+reporter.report(ws, projPath, report);
 ws.on('finish', function () {
    console.log('report saved at '+savePath);
 });
